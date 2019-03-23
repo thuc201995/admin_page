0d38965e52bbc8c2a9d7671d74487c8f0fff1578
thuc201995 <32479824+thuc201995@users.noreply.github.com>
2019-03-24 06:06:35 +0700

    initital config
    initital config

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+// index.js
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Route, Switch } from "react-router"; // react-router v4
+import { ConnectedRouter } from "connected-react-router";
+import configureStore, { history } from "./configureStore";
+const store = configureStore(/* provide initial state if any */);
 
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" render={() => <div>Match</div>} />
+        <Route exact path="/test" render={() => <div>test</div>} />
+        <Route exact path="/test2" render={() => <div>test2</div>} />
+        <Route render={() => <div>Miss</div>} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
+);
